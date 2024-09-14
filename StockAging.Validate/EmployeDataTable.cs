edf801e7c33f435899f47b589862504d9aedab67
Ayush Yadav <aayush0029@gmail.com>
2024-09-14 13:46:32 +0530

    Added EmployeDataTable.cs

---
+++ StockAging.Validate/EmployeDataTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAging.Validate
+{
+    public class EmployeDataTable
+    {   
+        public string Id {  get; set; }
+        public string Symbol { get; set; }
+        public string NetQuantity { get; set; }
+        public DateOnly First_Date { get; set; }
+        public DateOnly Last_Date { get; set; }
+        public int Days { get; set; }
+    }
+}
